44cb29a43b239e5b06d63f00ca6c5cdc75d389ab
Xiaoyuan Zhang <2605207z@student.gla.ac.uk>
2021-07-28 20:51:35 +0800

    finish 4.1,4.2

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    html="Rango says hey there partner!"+'<a href="/rango/about/">About</a>'
-    return HttpResponse(html)
+    #html="Rango says hey there partner!"+'<a href="/rango/about/">About</a>'
+    #return HttpResponse(html)
+
+    context_dict={'boldmessage':'Crunchy,creamy,cookie,candy,cupcake!'}
+    return render(request,'rango/index.html',context=context_dict)
     
 
 def about(request):
