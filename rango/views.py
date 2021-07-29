e0f52a680669f6df17e55220ce9e4d6d762f5893
Xiaoyuan Zhang <2605207z@student.gla.ac.uk>
2021-07-29 19:33:01 +0800

    finished chapter6 and start the test

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,21 @@
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from rango.models import Page
+from rango.models import Category
+
 
 def index(request):
     #html="Rango says hey there partner!"+'<a href="/rango/about/">About</a>'
     #return HttpResponse(html)
 
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list=Category.objects.order_by('-likes')[:5]
+    
+    context_dict={}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
     return render(request,'rango/index.html',context=context_dict)
     
 
@@ -14,3 +23,18 @@ def about(request):
     #return HttpResponse("Rango says here is the about page."+'<a href="/rango/">Index</a>')
     #context_dict={'boldmessage':'This tutorial has been put together by Xiaoyuan Zhang'}
     return render(request,'rango/about.html')
+
+def show_category(request,category_name_slug):
+
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages']=pages
+        context_dict['category']=category
+    except Category.DoesNotExist:
+        context_dict['category']=None
+        context_dict['pages']=None
+
+    return render(request,'rango/category.html',context=context_dict)
