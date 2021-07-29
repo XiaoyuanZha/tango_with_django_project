8e242821262ebfd7965cacc14328ce8857cf98d9
Xiaoyuan Zhang <2605207z@student.gla.ac.uk>
2021-07-29 21:34:15 +0800

    chapter 7 before the exercise

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 from rango.models import Page
 from rango.models import Category
 
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+
 
 def index(request):
     #html="Rango says hey there partner!"+'<a href="/rango/about/">About</a>'
@@ -40,3 +43,16 @@ def show_category(request,category_name_slug):
         context_dict['pages']=None
 
     return render(request,'rango/category.html',context=context_dict)
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request,'rango/add_category.html',{'form':form})
