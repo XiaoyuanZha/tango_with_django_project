f38246c21adcee836a126f31ba41b1a37415bcbe
Xiaoyuan Zhang <2605207z@student.gla.ac.uk>
2021-07-29 23:22:21 +0800

    change add before but forget the commit

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,8 @@ from rango.models import Category
 from rango.forms import CategoryForm
 from django.shortcuts import redirect
 
+from rango.forms import PageForm
+from django.urls import reverse
 
 def index(request):
     #html="Rango says hey there partner!"+'<a href="/rango/about/">About</a>'
@@ -56,3 +58,30 @@ def add_category(request):
         else:
             print(form.errors)
     return render(request,'rango/add_category.html',{'form':form})
+
+def add_page(request, category_name_slug):
+        try:
+            category = Category.objects.get(slug=category_name_slug)
+        except Category.DoesNotExist:
+            category = None
+        
+        if category is None:
+            return redirect('/rango/')
+
+        form = PageForm()
+
+        if request.method == 'POST':
+            form = PageForm(request.POST)
+
+            if form.is_valid():
+                if category:
+                    page = form.save(commit=False)
+                    page.category = category
+                    page.views = 0
+                    page.save()
+                    
+                    return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+            else:
+                print(form.errors)
+        context_dict = {'form': form,'category':category}
+        return render(request, 'rango/add_page.html', context=context_dict)
