a4f84a68cd14dacdeb20538404f7bcf999c74fd5
XiaoyuanZha <2605207z@student.gla.ac.uk>
2021-07-30 17:22:22 +0800

    chapter 9 before test

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,10 @@ from rango.forms import CategoryForm
 from rango.forms import PageForm,UserForm,UserProfileForm
 from django.urls import reverse
 
+from django.contrib.auth import authenticate, login,logout
+
+from django.contrib.auth.decorators import login_required
+
 def index(request):
     #html="Rango says hey there partner!"+'<a href="/rango/about/">About</a>'
     #return HttpResponse(html)
@@ -112,3 +116,32 @@ def register(request):
         profile_form = UserProfileForm()
 
     return render(request, 'rango/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered}) 
+
+def user_login(request):
+    if request.method =="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_activate:
+                login(request,user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse('Your Rango account is disabled')
+        else:
+            print(f"Invalid login details: {username},{password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+
+    return redirect(reverse('rango:index'))
